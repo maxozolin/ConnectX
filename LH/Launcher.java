7368caa50f38d30ea33b4b4c779123557c56bc8b
Max <NaN>
2023-04-03 11:25:56 +0200

    jvm attacher

--- LH/Launcher.java
+++ LH/Launcher.java
@@ -1,4 +1,7 @@
 import com.sun.tools.attach.VirtualMachine;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
 import java.lang.instrument.Instrumentation;
 
 public class Launcher {
@@ -7,6 +10,18 @@ public class Launcher {
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
+
+      String className = "connectx.CXBoard"; 
+      // load the bytecode from the .class file
+      try{
+        Class clazz = Class.forName(className); // the class to reload
+        URL url = new URL(clazz.getProtectionDomain().getCodeSource().getLocation(),
+        className.replace(".", "/") + ".class");
+        InputStream classStream = url.openStream();
+        byte[] bytecode = IOUtils.toByteArray(classStream);
+      } catch (Exception e){
+        System.out.println(e.getMessage());
+      }
       System.out.println("Hi agenmain");
       System.out.println(inst.isRedefineClassesSupported());
 
