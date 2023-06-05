076ebb7dd8fc9b816ad659c7a88bb143dfb321e2
Max <NaN>
2023-06-05 21:25:08 +0200

    lh

--- LH/writtenagent/agent/TransformerServiceMBean.java
+++
@@ -1,9 +0,0 @@
-public interface TransformerServiceMBean {
-  /**
-	 * Transforms the target class name
-	 * @param className The binary name of the target class
-	 * @param methodName  The name of the method to transform
-	 * @param methodSignature A regular expression to match the method signature. (if null, matches ".*")
-	 */
-	public void transformClass(String className, String methodName, String methodSignature);
-}
